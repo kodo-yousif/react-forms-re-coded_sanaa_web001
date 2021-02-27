c88d7a9c2a2bc1da9b7dab8cba71ef763457c915
Kodo Yousif <kodo.00257248@gmail.com>
2021-02-27 06:31:55 +0000

    Automatically backed up by Learn

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class DisplayData extends React.Component {
+export default class Data extends React.Component {
   render() {
     return (
       <div>
