78958c5aedf0b90fce9452c55669e1c9c3c5afa7
Kodo Yousif <kodo.00257248@gmail.com>
2021-02-27 06:26:07 +0000

    Automatically backed up by Learn

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -4,8 +4,8 @@ export default class DisplayData extends React.Component {
   render() {
     return (
       <div>
-        <h1>{this.props.formData.firstName}</h1>
-        <h1>{this.props.formData.lastName}</h1>
+        <h1>{this.props.formData.FName}</h1>
+        <h1>{this.props.formData.LName}</h1>
       </div>
     )
   }
